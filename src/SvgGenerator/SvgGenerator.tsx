281980fcdc5ff5a69ad52304622516009aefb0e6
Кирилл Дегтярев <kirill.degtyar76@gmail.com>
2023-01-14 00:48:11 +0300

    changes

--- src/SvgGenerator/SvgGenerator.tsx
+++ src/SvgGenerator/SvgGenerator.tsx
@@ -14,11 +14,12 @@ const SvgGenerator: React.FC<SvgGeneratorProps> = ({ id, color, width, height })
         case 'pawn':
             return (
                 <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={`${styles['figure-icon']} ${styles[color]}`}
+                    viewBox="0 0 100 100"
                     height={`${height}px`}
                     width={`${width}px`}
-                    viewBox="0 0 100 100"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className={`${styles['figure-icon']} ${styles[color]}`}>
+                    id={`${id}-${color}`}>
                     <path d="M37,38c0-1.1,0.9-2,2-2h22c1.1,0,2,0.9,2,2s-0.9,2-2,2H39C37.9,40,37,39.1,37,38z M34,84h32c1.1,0,2-0.9,2-2s-0.9-2-2-2H34   c-1.1,0-2,0.9-2,2S32.9,84,34,84z M69,85H31c-2.2,0-4,1.8-4,4s1.8,4,4,4h38c2.2,0,4-1.8,4-4S71.2,85,69,85z M50,35   c7.18,0,13-5.82,13-13S57.18,9,50,9s-13,5.82-13,13S42.82,35,50,35z M58,41H42c0,33.478-4.052,33.959-5.99,38H63.99   C62.052,74.959,58,74.478,58,41z" />
                 </svg>
             );
--- src/components/Cell/CellComponent.tsx
+++ src/components/Cell/CellComponent.tsx
@@ -22,39 +22,39 @@ const CellComponent: React.FC<CellProps> = ({ cell, selected, click, currentPlay
         return className;
     };
 
-    // const dragStartHandler = (e: React.DragEvent<HTMLDivElement>, cell: Cell) => {
-    //     console.log('start');
-    //     click(cell);
-    // };
-    // const dragEndHandler = (e: React.DragEvent<HTMLDivElement>) => {
-    //     console.log('end');
-    //     console.log('2');
-    // };
-    // const dragOverHandler = (e: React.DragEvent<HTMLDivElement>, cell: Cell) => {
-    //     console.log(cell);
-    //     e.preventDefault();
-    // };
-    // const dropHandler = (e: React.DragEvent<HTMLDivElement>, cell: Cell) => {
-    //     e.preventDefault();
-    //     console.log('drop');
-    //     click(cell);
-    // };
+    const dragStartHandler = (e: React.DragEvent<HTMLDivElement>, cell: Cell) => {
+        // e.dataTransfer.setData('text', e.currentTarget.children[0].id);
+        // console.log(e.dataTransfer.getData('text'));
+        // e.dataTransfer.clearData();
+        console.log(e);
+
+        click(cell);
+    };
+    const dragEndHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        // console.log('2');
+    };
+    const dragOverHandler = (e: React.DragEvent<HTMLDivElement>, cell: Cell) => {
+        e.preventDefault();
+    };
+    const dropHandler = (e: React.DragEvent<HTMLDivElement>, cell: Cell) => {
+        e.preventDefault();
+        click(cell);
+    };
 
     return (
         <div
             className={getClass(cell, selected)}
             onClick={() => click(cell)}
-            // onDragStart={(e) => dragStartHandler(e, cell)}
-            // onDragLeave={(e) => dragEndHandler(e)}
-            // onDragEnd={(e) => dragEndHandler(e)}
-            // onDragOver={(e) => dragOverHandler(e, cell)}
-            // onDrop={(e) => dropHandler(e, cell)}
-            // draggable={cell.board.startGame}
-        >
+            onDragLeave={(e) => dragEndHandler(e)}
+            onDragEnd={(e) => dragEndHandler(e)}
+            onDragOver={(e) => dragOverHandler(e, cell)}
+            onDrop={(e) => dropHandler(e, cell)}>
             {cell.x === 0 && <span className={styles.number}>{cell.y + 1}</span>}
             {cell.available && !cell.figure && <div className={styles.available}></div>}
             {cell.figure && (
                 <div
+                    onDragStart={(e) => dragStartHandler(e, cell)}
+                    draggable={cell.board.startGame}
                     className={`${
                         currentPlayer?.color === cell?.figure.color && cell.board.startGame
                             ? styles['current-figure']
